b7972ba996316610ea2ed6daff092130d840a806
Shlok Jain <akashlok238@gmail.com>
2023-10-29 13:12:40 +0530

    committed

---
+++ main.py
@@ -0,0 +1,120 @@
+from kivy.core.text import LabelBase
+from kivy.uix.screenmanager import ScreenManager
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.menu import MDDropdownMenu
+from kivy.properties import ObjectProperty
+from kivymd.uix.scrollview import MDScrollView
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.metrics import dp
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.core.window import Window
+from otp_generate import *
+from connection import *
+from authentication import *
+
+Window.size = (310, 580)
+
+class CustomTopAppBar(MDScreen):
+    pass
+
+class HelpDroid(MDApp):
+    def build(self):
+        self.theme_cls.primary_palette = "Teal"
+        self.theme_cls.theme_style = "Dark"
+        screen_manager=ScreenManager()
+        screen_manager.add_widget(Builder.load_file("main.kv"))
+        screen_manager.add_widget(Builder.load_file("login_pg.kv"))
+        screen_manager.add_widget(Builder.load_file("loginotp_pg.kv"))
+        screen_manager.add_widget(Builder.load_file("registration_pg.kv"))
+        screen_manager.add_widget(Builder.load_file("forgot_pg.kv"))
+        screen_manager.add_widget(Builder.load_file("welcome.kv"))
+        screen_manager.add_widget(Builder.load_file("edit_details_pg.kv"))
+        self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.accent_palette = "Gray"
+        self.theme_cls.primary_hue = "900"
+        return screen_manager
+    def viewmed(self):
+        self.root.current="viewmed"
+    def generateR(self):
+        self.temp_otp =  generate_otp()
+        email = self.root.get_screen("registration").ids.Email.text
+       
+        send_mail(email,self.temp_otp)
+    def generateF(self):
+        self.temp_otp =  generate_otp()
+        email = self.root.get_screen("forgot_password").ids.Email.text
+       
+        send_mail(email,self.temp_otp)
+    def generateO(self):
+        self.temp_otp =  generate_otp()
+        email = self.root.get_screen("loginwithotp").ids.Email.text
+       
+        send_mail(email,self.temp_otp)
+    
+    def register(self):
+        email = self.root.get_screen("registration").ids.Email.text
+        password = self.root.get_screen("registration").ids.password.text
+        name = self.root.get_screen("registration").ids.Name.text
+        mobile = self.root.get_screen("registration").ids.MobileNo.text
+        otp = self.root.get_screen("registration").ids.OTP.text
+        check=reg_auth(email,otp,password,name,mobile,self.temp_otp)
+        if(check):
+            print("signed in successful")
+            exec(open("HelpDroid\test.py").read())
+           # self.root.current="welcome"
+            #new page call
+        else:
+            self.root.get_screen("registration").ids.Email.text = ''
+            self.root.get_screen("registration").ids.password.text = ''
+            self.root.get_screen("registration").ids.Name.text = ''
+            self.root.get_screen("registration").ids.MobileNo.text = ''
+            self.root.get_screen("registration").ids.OTP.text = ''
+            print("signed in unsuccessful")
+    def login(self):
+        email = self.root.get_screen("login").ids.username.text
+        password = self.root.get_screen("login").ids.password.text
+        if(login_auth(email,password)):
+            print("Login Successful")
+            self.root.current="welcome"
+        else:
+            print("Login declined")
+    def login_with_otp(self):
+        email = self.root.get_screen("loginwithotp").ids.Email.text
+        otp = self.root.get_screen("loginwithotp").ids.OTP.text
+        if(otp==self.temp_otp):
+            
+            if(loginotpcheck(email)):
+                self.root.current="welcome"
+                print("Login Successful")
+            else:
+                self.root.get_screen("loginwithotp").ids.Email.text = ''
+                self.root.get_screen("loginwithotp").ids.OTP.text = ''
+                print("Login Failed")
+            #
+        else:
+            self.root.get_screen("loginwithotp").ids.Email.text = ''
+            self.root.get_screen("loginwithotp").ids.OTP.text = ''
+            print("otp wrong")
+    def forgot_password(self):
+        email = self.root.get_screen("forgot_password").ids.Email.text
+        otp = self.root.get_screen("forgot_password").ids.OTP.text
+        password = self.root.get_screen("forgot_password").ids.password.text
+        if(otp==self.temp_otp):
+            hash = sha256(password+""+email)    
+            update(email,hash)
+            self.root.current="login"
+            print('Password changed')
+        else:
+            self.root.get_screen("forgot_password").ids.OTP.text = ''
+            self.root.get_screen("forgot_password").ids.password.text = ''
+            self.root.get_screen("forgot_password").ids.Email.text = ''
+            print("otp wrong")
+    def editdetails(self):
+        self.root.current="editdetails"
+    def logout(self):
+        self.root.current="login"
+
+if __name__ == "__main__":
+    HelpDroid().run()
--- test (2).py
+++ test (2).py
@@ -179,3 +179,189 @@ class Example(MDApp):
     #def logout(self):
 
 Example().run()
+
+
+
+
+
+MDNavigationLayout:
+
+        MDScreenManager:
+
+            MDScreen:
+                name: "dashboard"
+                MDTopAppBar:
+                    title: "Welcome to Helpdroid"
+                    elevation: 3
+                    pos_hint: {"top": 1}
+                    font_style: "H2"
+                    specific_text_color: app.theme_cls.primary_color
+                    md_bg_color: app.theme_cls.accent_color
+                    left_action_items: [["menu", lambda x: app.root.ids.nav_drawer.set_state("open")]]
+                    right_action_items:
+                        [
+                        ["home", lambda x: app.on_home_button(x), "", "Home"],
+                        ["logout", lambda x: app.logout(), "", "Logout"],
+                        ["account-edit-outline", lambda x: app.callback(x), "" , "Edit details"],
+                        ]
+                
+                MDLabel:
+                    text: "Welcome Onboard"
+                    halign: "center"
+            MDScreen:
+                name: "doctor"
+                MDTopAppBar:
+                    title: "Welcome to Helpdroid"
+                    elevation: 3
+                    pos_hint: {"top": 1}
+                    font_style: "H2"
+                    specific_text_color: app.theme_cls.primary_color
+                    md_bg_color: app.theme_cls.accent_color
+                    left_action_items: [["menu", lambda x: app.root.ids.nav_drawer.set_state("open")]]
+                    right_action_items:
+                        [
+                        ["home", lambda x: app.on_home_button(x), "", "Home"],
+                        ["logout", lambda x: app.logout(), "", "Logout"],
+                        ["account-edit-outline", lambda x: app.callback(x), "" , "Edit details"],
+                        ]
+                
+                MDLabel:
+                    text: "Welcome Doctor"
+                    halign: "center"
+            MDScreen:
+                name: "medication"
+                MDTopAppBar:
+                    title: "Welcome to Helpdroid"
+                    elevation: 3
+                    pos_hint: {"top": 1}
+                    font_style: "H2"
+                    specific_text_color: app.theme_cls.primary_color
+                    md_bg_color: app.theme_cls.accent_color
+                    left_action_items: [["menu", lambda x: app.root.ids.nav_drawer.set_state("open")]]
+                    right_action_items:
+                        [
+                        ["home", lambda x: app.on_home_button(x), "", "Home"],
+                        ["logout", lambda x: app.logout(), "", "Logout"],
+                        ["account-edit-outline", lambda x: app.callback(x), "" , "Edit details"],
+                        ]
+                
+                FloatLayout:
+                    size_hint: None, None
+                    size: dp(300), dp(250)
+                    pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+
+                    MDRaisedButton:
+                        text: "Upload Prescription"
+                        pos_hint: {'center_x': 0, 'center_y': 0.5}
+                        md_bg_color: app.theme_cls.primary_color
+                        on_release: app.generateO()
+                    MDRaisedButton:
+                        id: button
+                        text: "View Uploaded Prescription"
+                        pos_hint: {"center_x": 1, "center_y": .5}
+                        on_release: app.menu_open()
+        MDNavigationDrawer:
+            id: nav_drawer
+            radius: (0, 16, 16, 0)
+
+            MDNavigationDrawerMenu:
+
+                MDNavigationDrawerHeader:
+                    title: "Menu"
+                    title_color: app.theme_cls.primary_color
+                    spacing: "4dp"
+                    padding: "12dp", 0, 0, "56dp"
+                    
+                    
+                DrawerClickableItem:
+                    icon: "doctor"
+                    text: "Doctor"
+                    on_press:
+                        app.root.ids.nav_drawer.set_state("close")
+                        app.root.ids.screen_manager.current = "doctor"
+
+                DrawerClickableItem:
+                    icon: "medication"
+                    text: "Medical Data"
+                    on_press:
+                        app.root.ids.nav_drawer.set_state("close")
+                        app.root.ids.screen_manager.current = "medication"
+
+                DrawerClickableItem:
+                    icon: "heart-pulse"
+                    text: "Arduino"
+                    on_press:
+                        app.root.ids.nav_drawer.set_state("close")
+                        app.root.ids.screen_manager.current = "arduino"
+
+                DrawerClickableItem:
+                    icon: "bell-ring"
+                    text: "Notification"
+                    on_press:
+                        app.root.ids.nav_drawer.set_state("close")
+                        app.root.ids.screen_manager.current = "notification"
+
+                MDNavigationDrawerDivider:
+
+                DrawerClickableItem:
+                    icon: "logout"
+                    text: "Logout"
+                    on_press : app.logout()
+
+
+
+
+MDScreen:
+
+    MDNavigationLayout:
+
+        MDScreenManager:
+
+            MDScreen:
+
+                MDTopAppBar:
+                    title: "Navigation Drawer"
+                    elevation: 4
+                    pos_hint: {"top": 1}
+                    md_bg_color: "#e7e4c0"
+                    specific_text_color: "#4a4939"
+                    left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
+
+        MDNavigationDrawer:
+            id: nav_drawer
+            radius: (0, 16, 16, 0)
+
+            MDNavigationDrawerMenu:
+
+                MDNavigationDrawerHeader:
+                    title: "Header title"
+                    title_color: "#4a4939"
+                    text: "Header text"
+                    spacing: "4dp"
+                    padding: "12dp", 0, 0, "56dp"
+
+                MDNavigationDrawerLabel:
+                    text: "Mail"
+
+                DrawerClickableItem:
+                    icon: "gmail"
+                    right_text: "+99"
+                    text_right_color: "#4a4939"
+                    text: "Inbox"
+
+                DrawerClickableItem:
+                    icon: "send"
+                    text: "Outbox"
+
+                MDNavigationDrawerDivider:
+
+                MDNavigationDrawerLabel:
+                    text: "Labels"
+
+                DrawerLabelItem:
+                    icon: "information-outline"
+                    text: "Label"
+
+                DrawerLabelItem:
+                    icon: "information-outline"
+                    text: "Label"
