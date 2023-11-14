e86c691ce1d419de2dd2cc8878a68cf85776f64f
akankshamaloo <akanksha.maloo.it24@heritageit.edu.in>
2023-11-14 18:28:35 +0530

    donewithlife

--- knn_predict.py
+++ knn_predict.py
@@ -2,7 +2,9 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import pickle
+import random
 from sklearn.metrics import classification_report
+from ar import *
 
 
 
@@ -12,8 +14,16 @@ model_pkl_file = "knn_weight.pkl"
 with open(model_pkl_file, 'rb') as file:  
     model = pickle.load(file)
 
-arr=[100.6,107,18,136,68,98]
+p=arduino()
+arr=[]
+arr.append(round(random.uniform(97.7,98.9),2))
+arr.append(p[0])
+arr.append(round(random.uniform(12,16),2))
+arr.append(round(random.uniform(100,120),2))
+arr.append(round(random.uniform(60,80),2))
+arr.append(p[1])
 
+print(arr)
 # evaluate model 
 print(model.predict(arr)[0])
 
